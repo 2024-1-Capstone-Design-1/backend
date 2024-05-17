a1495dd2da9dfc60d9014b09f461a9b506d759a1
Mingyu Kim <76204178+2K0313@users.noreply.github.com>
2024-05-18 05:15:09 +0900

    Feat: add login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 import logger from "../utils/logger.js";
-import { createUser } from "../services/authService.js";
+import { createUser, loginService } from "../services/authService.js";
 
 async function signup(req, res) {
   try {
@@ -15,7 +15,7 @@ async function signup(req, res) {
 
     if (missingFields.length > 0) {
       logger.debug(
-        `signup(controller): Missing required fields: ${missingFields.join(
+        `signup(authController): Missing required fields: ${missingFields.join(
           ", "
         )}`
       );
@@ -27,14 +27,49 @@ async function signup(req, res) {
 
     return res.status(result.status).json({ message: result.message });
   } catch (err) {
-    logger.error(`signup(controller): ${err.message}`);
+    logger.error(`signup(authController): ${err.message}`);
 
     if (err.status == 500) {
-      return res.status(err.status).json({ message: `Internal Server Error` });
+      return res.status(err.status).json({ message: err.message });
     }
 
     return res.status(err.status).json({ message: err.message });
   }
 }
 
-export { signup };
+async function login(req, res) {
+  try {
+    const loginData = req.body;
+    const dbClient = req.dbClient;
+    const missingFields = [];
+
+    if (!loginData.email) missingFields.push("email");
+    if (!loginData.password) missingFields.push("password");
+
+    if (missingFields.length > 0) {
+      logger.debug(
+        `login(authController): Missing required fields: ${missingFields.join(
+          ", "
+        )}`
+      );
+
+      return res.status(400).json({ message: `Missing required fields` });
+    }
+
+    const result = await loginService(loginData, dbClient);
+
+    return res
+      .status(result.status)
+      .json({ message: result.message, data: result.data });
+  } catch (err) {
+    logger.error(`login(authController): ${err.message}`);
+
+    if (err.status == 500) {
+      return res.status(err.status).json({ message: err.message });
+    }
+
+    return res.status(err.status).json({ message: err.message });
+  }
+}
+
+export { signup, login };
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 import logger from "../utils/logger.js";
 import AppError from "../utils/appError.js";
@@ -21,7 +22,7 @@ async function createUser(userData, dbClient) {
     );
 
     if (existingUser.rows.length > 0) {
-      logger.debug(`createUser(service): Email(${email}) already exists`);
+      logger.debug(`createUser(authService): Email(${email}) already exists`);
 
       throw new AppError(400, `Email already exists`);
     } else {
@@ -33,20 +34,124 @@ async function createUser(userData, dbClient) {
       const userId = result.rows[0].id;
 
       logger.debug(
-        `createUser(service): New user(${email}) created with ID(${userId})`
+        `createUser(authService): New user(${email}) created with ID(${userId})`
       );
     }
 
     return AppResponse(201, `User created successfully`);
   } catch (err) {
-    logger.error(`createUser(service): ${err.message}`);
+    logger.error(`createUser(authService): ${err.message}`);
 
     if (err.status !== 500) {
       throw err;
     }
 
-    throw new AppError(500, err.message);
+    throw new AppError(500, `Internal Server Error`);
   }
 }
 
-export { createUser };
+async function loginService(loginData, dbClient) {
+  try {
+    const { email, password } = loginData;
+
+    const existingUser = await dbClient.query(
+      `SELECT email, password, role FROM users WHERE email = $1`,
+      [email]
+    );
+
+    if (existingUser.rows.length == 0) {
+      logger.debug(
+        `loginService(authService): User with email(${email}) does not exist`
+      );
+
+      throw new AppError(404, `User does not exist`);
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.rows[0].password
+    );
+
+    if (!isPasswordValid) {
+      logger.debug(
+        `loginService(authService): Invalid password for user with email(${email})`
+      );
+
+      throw new AppError(401, `Invaild email or password`);
+    }
+
+    const accessToken = await generateAccessToken(existingUser);
+    const refreshToken = await generateRefreshToken(existingUser);
+
+    logger.debug(
+      `loginService(authService): User with email(${email}) successfully logged in`
+    );
+
+    return new AppResponse(200, "Login successful", {
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    logger.error(`loginService(authService): ${err.message}`);
+
+    if (err.status !== 500) {
+      throw err;
+    }
+
+    throw new AppError(500, `Internal Server Error`);
+  }
+}
+
+async function generateAccessToken(user) {
+  try {
+    const accessToken = jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_ACCESS_EXPIRATION,
+      }
+    );
+
+    if (!accessToken) {
+      logger.error(
+        `generateAccessToken(authService): Failed to generate access token`
+      );
+    }
+
+    logger.debug(
+      `generateAccessToken(authService): Access token generated for userid(${user.id})`
+    );
+
+    return accessToken;
+  } catch (err) {
+    logger.error(`generateAccessToken(authService): ${err.message}`);
+
+    throw new AppError(500, `Internal Server Error`);
+  }
+}
+
+async function generateRefreshToken(user) {
+  try {
+    const refreshToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_REFRESH_EXPIRATION,
+    });
+
+    if (!refreshToken) {
+      logger.error(
+        `generaterefreshToken(authService): Failed to generate refresh token`
+      );
+    }
+
+    logger.debug(
+      `generaterefreshToken(authService): Refresh token generated for userid(${user.id})`
+    );
+
+    return refreshToken;
+  } catch (err) {
+    logger.error(`generaterefreshToken(authService): ${err.message}`);
+
+    throw new AppError(500, `Internal Server Error`);
+  }
+}
+
+export { createUser, loginService };
