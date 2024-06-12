a7441c2579f982f801a2167606f2b27f7812d8c7
Mingyu Kim <mlnudg2000@gmail.com>
2024-06-12 10:18:25 +0900

    Feat: add update user data

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,9 @@
 import logger from "../utils/logger.js";
-import { getUserById, getProfileService } from "../services/userService.js";
+import {
+  getUserById,
+  getProfileService,
+  updateUserService,
+} from "../services/userService.js";
 
 async function getProfile(req, res) {
   try {
@@ -50,4 +54,66 @@ async function getProfile(req, res) {
   }
 }
 
-export { getProfile };
+async function updateUserController(req, res) {
+  try {
+    const user = req.user;
+
+    if (!user) {
+      logger.debug(
+        "updateUserController(userController): Unauthorized access attempt"
+      );
+
+      return res.status(401).json({ message: `Unauthorized access attempt` });
+    }
+
+    const dbClient = req.dbClient;
+    const existingUser = await getUserById(user.id, dbClient);
+
+    if (!existingUser) {
+      logger.debug(
+        `updateUserController(userController): User with id(${user.id}) does not exist`
+      );
+
+      return res.status(404).json({ message: "User does not exist" });
+    }
+
+    if (
+      user.id !== existingUser.id &&
+      user.email == existingUser.email &&
+      user.role == existingUser.role
+    ) {
+      logger.debug(
+        `updateUserController(userController): User data mismatch - token user(${user.id}, ${user.email}, ${user.role}) and db user(${existingUser.id}, ${existingUser.email}, ${existingUser.role})`
+      );
+
+      return res.status(403).json({ message: "User data mismatch" });
+    }
+
+    const userId = user.id;
+    const userUpdateData = req.body;
+
+    if (userUpdateData.length == 0) {
+      logger.debug(
+        `updateUserController(userController): Missing required fields`
+      );
+
+      return res.status(400).json({ message: `Missing required fields` });
+    }
+
+    const result = await updateUserService(userUpdateData, userId, dbClient);
+
+    return res
+      .status(result.status)
+      .json({ message: result.message, data: result.data });
+  } catch (err) {
+    logger.error(`updateUserController(userController): ${err.message}`);
+
+    if (err.status == 500) {
+      return res.status(err.status).json({ message: err.message });
+    }
+
+    return res.status(err.status).json({ message: err.message });
+  }
+}
+
+export { getProfile, updateUserController };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,18 +5,21 @@ import {
   releaseDbClient,
 } from "../middlewares/dbMiddleware.js";
 import authenticateToken from "../middlewares/authMiddleware.js";
-import { getProfile } from "../controllers/userController.js";
+import {
+  getProfile,
+  updateUserController,
+} from "../controllers/userController.js";
 
 const userRouter = Router();
 
 /**
  * @swagger
- * /get-profile:
+ * /user/get-profile:
  *   get:
  *     summary: Retrieve user profile
  *     description: Fetches the profile of the currently authenticated user.
  *     tags:
- *       - User
+ *       - Users
  *     security:
  *       - bearerAuth: []
  *     responses:
@@ -91,6 +94,115 @@ userRouter.get(
   authenticateToken,
   attachDbClient,
   getProfile,
+  releaseDbClient
+);
+
+/**
+ * @swagger
+ * /user/update:
+ *   patch:
+ *     summary: Update user information
+ *     description: Update the authenticated user's information such as nickname, profile image, and password.
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nickname:
+ *                 type: string
+ *                 description: The new nickname of the user
+ *                 example: "newNickname"
+ *               profileImage:
+ *                 type: string
+ *                 description: The new profile image URL of the user
+ *                 example: "http://example.com/new-profile.jpg"
+ *               password:
+ *                 type: object
+ *                 properties:
+ *                   current:
+ *                     type: string
+ *                     description: The current password of the user
+ *                     example: "password123"
+ *                   change:
+ *                     type: string
+ *                     description: The new password for the user
+ *                     example: "newPassword123"
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User updated successfully
+ *                 data:
+ *                   type: object
+ *                   example: {}
+ *       400:
+ *         description: Missing required fields
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Missing required fields
+ *       401:
+ *         description: Unauthorized access attempt
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Unauthorized access attempt
+ *       403:
+ *         description: User data mismatch
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User data mismatch
+ *       404:
+ *         description: User does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User does not exist
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal Server Error
+ */
+userRouter.patch(
+  "/update",
+  authenticateToken,
+  attachDbClient,
+  updateUserController,
   releaseDbClient
 );
 
